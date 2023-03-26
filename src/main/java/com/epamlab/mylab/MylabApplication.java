b325d4428f6dd3b057ac8e588ec7f2318cd36b52
Pixel <my>
2023-03-26 23:19:38 +0300

    First Commit

---
+++ src/main/java/com/epamlab/mylab/MylabApplication.java
@@ -0,0 +1,14 @@
+package com.epamlab.mylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylabApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/epamlab/mylab/MylabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epamlab.mylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
